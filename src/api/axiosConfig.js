9dfeb87367fffdea21ac80ea0e3107615bab197a
OpamLNL <nataliya.linchuk@uzhnu.edu.ua>
2025-03-16 10:06:07 +0200

    axios fix

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,51 @@
-//axiosConfig.js
-
-
 import axios from 'axios';
 import { apiBaseURL } from '../configs/urls';
+
 export const axiosInstance = axios.create({
-    baseURL: apiBaseURL
+    baseURL: apiBaseURL,
+    headers: {
+        'Accept': 'application/json',
+    },
 });
 
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const accessToken = localStorage.getItem('accessToken');
+
+                if (!accessToken) {
+                    return Promise.reject(error);
+                }
+
+                const response = await axios.post('/api/auth/refresh-token', { accessToken });
+
+                const newAccessToken = response.data.accessToken;
+                localStorage.setItem('accessToken', newAccessToken);
+
+                originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                return axiosInstance(originalRequest);
+            } catch (refreshError) {
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
